70f54c27c7709c009a3622d4dbe9b375edf4118a
DaPasta <484902@gmail.com>
2015-11-24 01:16:35 -0800

    Adds base robot project

---
+++ src/org/team1515/robama/OI.java
@@ -0,0 +1,10 @@
+package org.team1515.robama;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+}
+
