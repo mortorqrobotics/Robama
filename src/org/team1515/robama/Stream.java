e2e2b6fc2616fd55586876cd6e139852c57a3b58
voidpigeon <thevoidpigeon@gmail.com>
2016-03-31 09:34:08 -0700

    Add lines to stream

--- src/org/team1515/robama/Stream.java
+++ src/org/team1515/robama/Stream.java
@@ -1,9 +1,15 @@
 package org.team1515.robama;
 
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
 import com.ni.vision.NIVision.Image;
 import com.ni.vision.NIVision.ImageInfo;
 import com.ni.vision.NIVision.ScalingMode;
+import com.ni.vision.NIVision.ShapeMode;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -14,6 +20,10 @@ public class Stream extends Command {
 	
 	int session;
 	Image frame;
+	Map<Integer, Color> lines;
+	
+	static final int TICK_HEIGHT = 2;
+	static final int TICK_PERIOD = 6;
 	
 	final int DELAY = 100; // DELAY = 1000 / FPS
 	final int SCALE = 8; // sizes of both axes are divided by this
@@ -22,6 +32,9 @@ public class Stream extends Command {
 	
 	public Stream() {
         frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+        
+        lines = new HashMap<Integer, Color>();
+        lines.put(10, Color.RED);
 	}
 
 	@Override
@@ -35,12 +48,28 @@ public class Stream extends Command {
 	@Override
 	protected void execute() {
 		long now = System.currentTimeMillis();
-		if(now - lastUpdate >= DELAY) {
+		if (now - lastUpdate >= DELAY) {
 			lastUpdate += DELAY;
             NIVision.IMAQdxGrab(session, frame, 1);
             ImageInfo info = NIVision.imaqGetImageInfo(frame);
+            int width = info.xRes;
+            int height = info.yRes;
+            
             NIVision.Rect rect = new NIVision.Rect(0, 0, info.xRes, info.yRes);
             NIVision.imaqScale(frame, frame, SCALE, SCALE, ScalingMode.SCALE_SMALLER, rect);
+            
+            for (Integer x : lines.keySet()) {
+            	for (int i = 0; i < height / TICK_PERIOD; i++) {
+            		int y = i * TICK_PERIOD;
+            		NIVision.Rect rect1 = new NIVision.Rect(x, y, x + 1, y + TICK_HEIGHT);
+            		NIVision.Rect rect2 = new NIVision.Rect(width - x - 1, y, width - x, y + TICK_HEIGHT);
+            		NIVision.imaqDrawShapeOnImage(frame, frame, rect1,
+            				DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, lines.get(x).getRGB());
+            		NIVision.imaqDrawShapeOnImage(frame, frame, rect2,
+            				DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, lines.get(x).getRGB());
+            	}
+            }
+            
             CameraServer.getInstance().setImage(frame);
 		}
 	}
