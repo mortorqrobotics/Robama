44be4a89af0190bf8843252cb83014640439f0e9
voidpigeon <thevoidpigeon@gmail.com>
2016-03-31 18:27:02 -0700

    Lower wedge in lowbar auto

--- src/org/team1515/robama/commands/LowbarAuto.java
+++ src/org/team1515/robama/commands/LowbarAuto.java
@@ -1,41 +1,13 @@
 package org.team1515.robama.commands;
 
-import org.team1515.robama.Robot;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-import edu.wpi.first.wpilibj.command.Command;
-
-public class LowbarAuto extends Command {
-	
-	private static final double SPEED = 0.5;
+public class LowbarAuto extends CommandGroup {
 	
 	public LowbarAuto() {
-		requires(Robot.driveTrain);
-		setTimeout(6);
-	}
-
-	@Override
-	protected void initialize() {
-		Robot.driveTrain.setSpeed(SPEED, SPEED);
-	}
-
-	@Override
-	protected void execute() {
-		
-	}
-
-	@Override
-	protected boolean isFinished() {
-		return isTimedOut();
-	}
-
-	@Override
-	protected void end() {
-		Robot.driveTrain.stop();
-	}
-
-	@Override
-	protected void interrupted() {
-		end();
+		addSequential(new WedgeDown(), 0.5);
+		addSequential(new Delay(), 1);
+		addSequential(new DriveForwardAuto(0.5), 6);
 	}
 
 }
