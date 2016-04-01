933f884ad66442528b9e98bc802171ef607be1f0
voidpigeon <thevoidpigeon@gmail.com>
2016-03-31 18:23:49 -0700

    Adjust time in portcullis auto

--- src/org/team1515/robama/commands/PortcullisAuto.java
+++ src/org/team1515/robama/commands/PortcullisAuto.java
@@ -7,7 +7,7 @@ public class PortcullisAuto extends CommandGroup {
 	public PortcullisAuto() {
 		addSequential(new WedgeDown(), 0.5);
 		addSequential(new Delay(), 1);
-		addSequential(new DriveForwardAuto(0.75), 3.5);
+		addSequential(new DriveForwardAuto(0.75), 1.75);
 	}
 
 }
